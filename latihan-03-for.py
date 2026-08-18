de6323387972b1d33f2dfd0ba92483aac5bb8d9d
Dafi-Rh <dafigaming596@gmail.com>
2026-08-18 19:08:14 +0800

    Latihan python Day 3 - For

---
+++ latihan-03-for.py
@@ -0,0 +1,22 @@
+angka = 1
+
+while angka <= 10:
+    print(angka)
+    angka += 1
+
+
+for angka in range(1, 10):
+    print(angka)
+
+
+for angka in range(10, 1):
+    print(angka)
+
+
+for angka in range(0, 35, 7):
+    print(angka)
+
+
+for angka in range(3, 20):
+    if angka % 2 == 0:
+        print(angka)
