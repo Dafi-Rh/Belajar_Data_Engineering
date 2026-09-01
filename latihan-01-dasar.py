bba45dfc5aca5dc8deb2326a9d427760492e9787
Dafi-Rh <dafigaming596@gmail.com>
2026-09-01 20:49:31 +0800

    Add files via upload

---
+++ latihan-01-dasar.py
@@ -0,0 +1,69 @@
+a = 6
+b = 11
+c = 3
+
+print(a + c)
+print(a + b - c)
+print(a - c + b * c)
+print(a / c)
+print(b / a)
+print(c - b + b / a)
+print(b // a)
+print(a % b)
+print(a % c)
+print(a ** c)
+
+print(9 ** 999)
+
+# tadi lupa pakai tanda kutip
+# nama = jirjat
+nama = "jirjat"
+
+# tadi coba pakai spasi
+# tempat tinggal = "ny"
+rumah = "ny"
+
+print("halo nama saya,", nama, "umur saya", b, "tahun", "saya tinggal di", rumah)
+
+nama = input("nama : ")
+print("aku", nama)
+
+umu = input("umur : ")
+
+umur = input("umur : ")
+print(f"halo namaku {nama}, saat ini umurku {umur} tahun")
+
+umur = int(input("umur = "))
+print(f"halo namaku, {nama}, umurku {umur} tahun")
+
+print(umur + umur)
+
+umur = input("umur = ")
+print(umur + umur)
+
+tinggi = 171, 2
+umur = 23
+
+print(type(nama))
+print(type(umur))
+print(type(tinggi))
+
+print("hello world")
+
+a = 3
+b = "7"
+
+# tadi error karena beda tipe data
+# print(a + str(b))
+
+print(b + str(a))
+print(a + int(b))
+
+# tadi salah tulis
+# tinggi 170.8
+
+tinggi = 170.8
+print(type(tinggi))
+
+c = "7.5"
+print(a + float(c))
